24ea3a6bfed8525c29cc51cf37da49d21e260933
Rafael Santos de Araujo <rafaelsantosdearaujo@gmail.com>
2022-10-20 10:52:16 -0300

    initial configuration

---
+++ src/main/java/com/springlibrabryapi/librarycontrol/LibraryControlTestApplication.java
@@ -0,0 +1,13 @@
+package com.springlibrabryapi.librarycontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryControlTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryControlTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springlibrabryapi/librarycontrol/LibraryControlTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springlibrabryapi.librarycontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryControlTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
