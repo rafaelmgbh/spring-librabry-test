4d75a342eb3f310093e07fcb8bc31407e89a155e
Rafael Santos de Araujo <rafaelsantosdearaujo@gmail.com>
2022-10-28 11:47:36 -0300

    implements first authentication tests

---
+++ src/main/java/com/springlibrabryapi/librarycontrol/configs/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.springlibrabryapi.librarycontrol.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration // Classe de configuração do Spring
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().and().authorizeHttpRequests().anyRequest().permitAll().and().csrf().disable();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
